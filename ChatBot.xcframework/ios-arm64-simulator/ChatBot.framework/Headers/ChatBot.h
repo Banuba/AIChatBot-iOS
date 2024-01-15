a4c7b386bc803223afeb2484c01cea3702362537
Ivan Gulidov <ivan.gulidov@banuba.com>
2024-01-15 12:09:26 +0100

    0.0.1

---
+++ ChatBot.xcframework/ios-arm64-simulator/ChatBot.framework/Headers/ChatBot.h
@@ -0,0 +1,18 @@
+//
+//  ChatBot.h
+//  ChatBot
+//
+//  Created by Andrei Sak on 18.12.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatBot.
+FOUNDATION_EXPORT double ChatBotVersionNumber;
+
+//! Project version string for ChatBot.
+FOUNDATION_EXPORT const unsigned char ChatBotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatBot/PublicHeader.h>
+
+
